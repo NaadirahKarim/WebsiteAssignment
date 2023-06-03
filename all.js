7159a8458766ad246252863bc394d61b5bcd6750
NaadirahKarim <2427389@students.wits.ac.za>
2023-06-03 18:25:07 +0200

    Update- Made edits to the Homepage

--- all.js
+++ all.js
@@ -1,19 +1,14 @@
-//Search Bar
-const searchInputWrapper = document.querySelector(".search-input-wrapper");
-const searchInput = document.querySelector(".search-input input");
-const searchIcon = document.querySelector(".search-icon i");
-const closeIcon = document.querySelector(".search-input i");
-
-searchIcon.addEventListener("click", () =>{
-    searchIcon.parentElement.classList.add("change");
-    searchInputWrapper.classList.add("change");
-
-    setTimeout(() =>{
-        searchInput.focus();
-    }, 1000);
+//Appearacnce Animation
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }else{
+            entry.target.classList.remove("show");
+        }
+    });
 });
 
-closeIcon.addEventListener("click", () =>{
-    searchIcon.parentElement.classList.remove("change");
-    searchInputWrapper.classList.remove("change");    
-});
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((element) => observer.observe (element));
