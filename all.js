7086ba7ca164fd211e278ff9c1ec3dc8a0caea01
NaadirahKarim <2427389@students.wits.ac.za>
2023-06-04 15:26:08 +0200

    Update-added microformats and js scripts

--- all.js
+++ all.js
@@ -12,3 +12,5 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((element) => observer.observe (element));
+
+// DOM for navigations header
