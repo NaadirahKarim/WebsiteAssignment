833f447978b2b0e53ac04b900c77889bbeddcca6
NaadirahKarim <2427389@students.wits.ac.za>
2023-06-18 00:34:16 +0200

    Update- closed the API

--- API.js
+++ API.js
@@ -1,16 +1,16 @@
-const url = 'https://quotes15.p.rapidapi.com/quotes/random/';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '87d42d06c9msh8b8e6698aee6a80p135d2djsn1eede4d61587',
-		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
-	}
-};
+// const url = 'https://quotes15.p.rapidapi.com/quotes/random/';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '87d42d06c9msh8b8e6698aee6a80p135d2djsn1eede4d61587',
+// 		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+// 	}
+// };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
