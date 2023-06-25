b2da18ea2bb4a269aec94e192e63269cc3e4bb32
NaadirahKarim <2427389@students.wits.ac.za>
2023-06-25 16:44:32 +0200

    Updated- Fixed Nav.js

--- Nav.js
+++ Nav.js
@@ -11,14 +11,60 @@
 //     });
 // });
 
-
-
 // const hiddenElements = document.querySelectorAll(".hidden");
 // hiddenElements.forEach((element) => observer.observe (element));
 
 
 // DOM for navigations header
 
+const home = document.querySelector("#home");
+home.addEventListener("click", () => AccessHomePage());
+const profile = document.querySelector("#profile");
+profile.addEventListener("click", () => AccessProfilePage());
+const blog = document.querySelector("#blog");
+blog.addEventListener("click", () => AccessBlogPage());
+const design = document.querySelector("#design");
+design.addEventListener("click", () => AccessDesignPage());
+const essay = document.querySelector("#essay");
+essay.addEventListener("click", () => AccessEssayPage());
+
+let displayNavigation = document.getElementById("Navigation");
+let homePage = document.getElementById("HomePage");
+let homePageInnerHTML =
+`<section class="p-card">
+<section class="container">
+        <section class="content">
+            <section class="profileimg">
+           <p><img  src="./images/Profile.jpg" class="u-photo" alt="Profile Pic Icon"></p>     
+            </section>
+            <p class="p-name"><h1> Introducing Naadirah Karim... </h1></p> 
+            <h2>A Digital Arts Engineering Student</h2>
+            <a href="./profile.html" class="About-btn">MORE ABOUT ME</a>
+        </section>
+    </section>                  
+</section>`;
+
+homePage.innerHTML = homePageInnerHTML;
+
+function AccessHomePage(){
+    window.location.replace("https://naadirahkarim.github.io/WebsiteAssignment/index.html");
+}
+
+function AccessProfilePage(){
+    window.location.replace("https://naadirahkarim.github.io/WebsiteAssignment/profile.html");
+}
+
+function AccessBlogPage(){
+    window.location.replace("https://naadirahkarim.github.io/WebsiteAssignment/blog.html#gsc.tab=0");
+}
+
+function AccessDesignPage(){
+    window.location.replace("https://naadirahkarim.github.io/WebsiteAssignment/design.html#gsc.tab=0");
+}
+
+function AccessEssayPage(){
+    window.location.replace("https://naadirahkarim.github.io/WebsiteAssignment/essay.html");
+}
 
 // // Create the header element
 // const header = document.createElement('header');
